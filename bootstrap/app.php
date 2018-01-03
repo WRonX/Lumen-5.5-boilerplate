97f85375fb0d6dd041533f9ce7bc66a2f40477f1
WRonX <wronx@wronx.net>
2018-01-03 18:33:39 +0100

    Fixed not registered middleware problem

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -63,9 +63,10 @@ $app->singleton(
 //    App\Http\Middleware\ExampleMiddleware::class
 // ]);
 
-// $app->routeMiddleware([
-//     'auth' => App\Http\Middleware\Authenticate::class,
-// ]);
+ $app->routeMiddleware([
+     'auth' => App\Http\Middleware\Authenticate::class,
+     'json' => App\Http\Middleware\JsonMiddleware::class,
+ ]);
 
 /*
 |--------------------------------------------------------------------------
