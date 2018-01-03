606d34c961f57966df6246623618c4cbfb8e2efe
WRonX <wronx@wronx.net>
2018-01-03 17:09:57 +0100

    Added debugbar

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -81,6 +81,12 @@ $app->singleton(
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+if (env('APP_DEBUG')) {
+    $app->register(Barryvdh\Debugbar\LumenServiceProvider::class);
+    
+    $app->configure('debugbar');
+}
+
 
 /*
 |--------------------------------------------------------------------------
